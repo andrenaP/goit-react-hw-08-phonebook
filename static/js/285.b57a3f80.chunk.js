beaf1bdfc0f693d9c593940b275b44d6ad1b4354
andrenaP <58563562+andrenaP@users.noreply.github.com>
2023-09-06 06:53:20 +0000

    Deploying to gh-pages from @ andrenaP/goit-react-hw-08-phonebook@315bba86541558453ecb8a93b1a738ef35cd1ef5 🚀

---
+++ static/js/285.b57a3f80.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkgoit_react_hw_08_phonebook=self.webpackChunkgoit_react_hw_08_phonebook||[]).push([[285],{285:function(e,t,n){n.r(t);var o=n(184);t.default=function(){return(0,o.jsx)("h2",{children:"Register page"})}}}]);
+//# sourceMappingURL=285.b57a3f80.chunk.js.map
